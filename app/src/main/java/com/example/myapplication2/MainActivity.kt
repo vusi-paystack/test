8d1cb5b0b9b23e7befff2b26fd78e1ff28728216
vusi-paystack <vusi@paystack.com>
2025-03-27 13:19:49 +0200

    test push2

--- app/src/main/java/com/example/myapplication2/MainActivity.kt
+++ app/src/main/java/com/example/myapplication2/MainActivity.kt
@@ -33,7 +33,7 @@ class MainActivity : ComponentActivity() {
 @Composable
 fun Greeting(name: String, modifier: Modifier = Modifier) {
     Text(
-        text = "Hello $name!",
+        text = "Hello super user, $name!",
         modifier = modifier
     )
 }q
